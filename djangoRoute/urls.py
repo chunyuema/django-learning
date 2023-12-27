b5a326c68c389028f7f550a9e912a598a12045fc
Chunyue Ma <cma01@wesleyan.edu>
2023-12-27 08:24:31 -0800

    Redirect and reverse parsing from views instead of from template

--- djangoRoute/urls.py
+++ djangoRoute/urls.py
@@ -12,5 +12,11 @@ urlpatterns = [
     path('test/<int:a>/<int:b>', test, name = 'test'),
 
     # using regular expression
-    re_path(r'test2/(?P<a>\d+)/(?P<b>\d+)/', test, name = 'test2')
+    re_path(r'test2/(?P<a>\d+)/(?P<b>\d+)/', test, name = 'test2'),
+
+
+    # The following are examples of redirecting and reverse parsing through views
+    path('myredirect/', my_redirect),
+    path('touserlist/', redirect_to_user_list),
+    path('touserdetail/', redirect_to_user_detail),
 ]
--- djangoRoute/views.py
+++ djangoRoute/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from djangoRoute.models import *
+
 
 # Create your views here.
 def index(request):
     # method 2: return html files
     return render(request, "index.html")
+
 
 def user_list(request):
     print("Getting all the users ...")
@@ -29,4 +31,27 @@ def test(request, a, b):
     return HttpResponse(f'a:{a} - b:{b}')
 
 
+'''
+The following are examples of redirecting and reverse parsing through views
+- redirect: used for redirecting from one url to another
+- reverse: used to do reverse parsing
+    - note that if you declared namespace, please specify from the reverse
+'''
 
+
+def my_redirect(request):
+    # redirect to google site
+    return redirect('https://www.google.com')
+
+
+def redirect_to_user_list(request):
+    return redirect('/djangoRoute/userlist/')
+
+
+def redirect_to_user_detail(request):
+    # use reverse parsing here, note that we use namespace here
+    # this is because we have declared the namespace in the djangoProject1
+    # return redirect(reverse('djangoRoute:userdetail', args=(2,)))
+
+    # another way of passing in the argument through keyword
+    return redirect(reverse('djangoRoute:userdetail', kwargs={'user_id': 2}))
