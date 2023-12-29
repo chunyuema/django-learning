2c2994f7bb13cc4d7a01646f170850293a8ad631
Chunyue Ma <cma01@wesleyan.edu>
2023-12-29 08:44:32 -0800

    Use of for loop

--- djangoTemplate/views.py
+++ djangoTemplate/views.py
@@ -5,7 +5,10 @@ def djangoTemplateIndex(request):
     data = {
         "name": "chunyue",
         "age": 25,
-        "hobbies": ["chess", "coding"],
+        "hobbies": ["chess", "swimming", "coding"],
         "address": {"city": "seattle", "state": "WA"},
+        "foobar": [["1", "2", "3"],
+                   ["4", "5", "6"],
+                   ["7", "8", "9"]]
     }
     return render(request, "index-django-template.html", data)
