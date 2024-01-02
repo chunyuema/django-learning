8adb918249268b08575462f6010fc2b39fda2b9d
Chunyue Ma <cma01@wesleyan.edu>
2024-01-02 09:54:08 -0800

    Register models to manage from admin page

--- djangoModels/admin.py
+++ djangoModels/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from djangoModels.models import *
+
+# Register the models in order to manage the models
+admin.site.register(PersonModel)
+admin.site.register(UserModel)
