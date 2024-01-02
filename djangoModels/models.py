26cf1912eb22e39e6cd98171ffc2d6fe063e4f2f
Chunyue Ma <cma01@wesleyan.edu>
2024-01-02 09:47:55 -0800

    Create new models with fields

--- djangoModels/models.py
+++ djangoModels/models.py
@@ -7,3 +7,22 @@ class UserModel(models.Model):
     is_deleted = models.BooleanField(default=False)
 
 
+class PersonModel(models.Model):
+    # Create customized primary key instead of relying on default
+    uid = models.AutoField(auto_created=True, primary_key=True)
+
+    # db_index is set to true to make the column index
+    name = models.CharField(max_length=30, unique=True, db_index=True)  # name varchar(30)
+
+    # Use default to set the age
+    age = models.IntegerField(default=18)                # age int default 18
+
+    # Use text field (make it optional)
+    self_intro = models.TextField(null=True, blank=True)
+
+    # Use decimals (limit the number of decimal digits to 2)
+    height = models.DecimalField(max_digits=5, decimal_places=2, default=150.00)
+
+    is_deleted = models.BooleanField(default=False)
+
+
